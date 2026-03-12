2c07c76d11e00fcda622c737cecfd59b4c4463b5
Chuyue Zhang <zhangchuyue.zhang@mail.utoronto.ca>
2026-03-12 21:55:46 +0800

    load noto serif fonts on demand

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,13 +16,6 @@ export default function RootLayout() {
     NotoSansJP_400Regular: require('../assets/fonts/noto_sans/NotoSansJP-Regular.ttf'),
     NotoSansJP_600SemiBold: require('../assets/fonts/noto_sans/NotoSansJP-SemiBold.ttf'),
     NotoSansJP_700Bold: require('../assets/fonts/noto_sans/NotoSansJP-Bold.ttf'),
-    // Noto Serif SC/JP - \"case law\" and rules
-    NotoSerifSC_400Regular: require('../assets/fonts/noto_serif/NotoSerifSC-Regular.ttf'),
-    NotoSerifSC_600SemiBold: require('../assets/fonts/noto_serif/NotoSerifSC-SemiBold.ttf'),
-    NotoSerifSC_700Bold: require('../assets/fonts/noto_serif/NotoSerifSC-Bold.ttf'),
-    NotoSerifJP_400Regular: require('../assets/fonts/noto_serif/NotoSerifJP-Regular.ttf'),
-    NotoSerifJP_600SemiBold: require('../assets/fonts/noto_serif/NotoSerifJP-SemiBold.ttf'),
-    NotoSerifJP_700Bold: require('../assets/fonts/noto_serif/NotoSerifJP-Bold.ttf'),
   });
 
   if (!fontsLoaded) return null;
